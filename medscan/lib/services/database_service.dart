d9d6f2d6c7e264142e687f103aa40e552a645ca4
nashacp1211 <nashacp1211@gmail.com>
2026-09-12 15:48:26 +0530

    Add database and tts services

---
+++ medscan/lib/services/database_service.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/medicine.dart';
+
+class DatabaseService {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'smartmed.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE medicines(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            dosage TEXT,
+            time TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertMedicine(Medicine medicine) async {
+    final db = await database;
+    return await db.insert('medicines', medicine.toMap());
+  }
+
+  Future<List<Medicine>> getMedicines() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('medicines');
+    return List.generate(maps.length, (i) => Medicine.fromMap(maps[i]));
+  }
+}
---
+++ medscan/lib/services/tts_service.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TTSService {
+  final FlutterTts _flutterTts = FlutterTts();
+
+  TTSService() {
+    _initTTS();
+  }
+
+  void _initTTS() async {
+    await _flutterTts.setLanguage("en-US");
+    await _flutterTts.setSpeechRate(0.5);
+    await _flutterTts.setVolume(1.0);
+    await _flutterTts.setPitch(1.0);
+  }
+
+  Future<void> speak(String text) async {
+    if (text.isNotEmpty) {
+      await _flutterTts.speak(text);
+    }
+  }
+
+  Future<void> stop() async {
+    await _flutterTts.stop();
+  }
+}
