8b4216f51f37b3c6589c83449619bdc5a373dfd7
jannajaleel31 <jannajaleel31@gmail.com>
2026-09-14 09:15:19 +0530

    Add AppLanguage class for language management

--- medscan/lib/app_language.dart
+++ medscan/lib/app_language.dart
@@ -1,1 +1,40 @@
+```dart
+import 'package:flutter/material.dart';
+
+class AppLanguage {
+  static String selectedLanguage = 'English';
+
+  static const List<String> languages = [
+    'English',
+    'Malayalam',
+    'Hindi',
+    'Tamil',
+  ];
+
+  static void setLanguage(String language) {
+    selectedLanguage = language;
+  }
+
+  static String getLanguageCode() {
+    switch (selectedLanguage) {
+      case 'Malayalam':
+        return 'ml';
+
+      case 'Hindi':
+        return 'hi';
+
+      case 'Tamil':
+        return 'ta';
+
+      case 'English':
+      default:
+        return 'en';
+    }
+  }
+
+  static Locale getLocale() {
+    return Locale(getLanguageCode());
+  }
+}
+```
 
